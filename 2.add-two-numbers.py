d4176e0437618f93463a6891c97b667338683b07
yumeiJP <tk5138@email.kist.ed.jp>
2026-03-31 19:38:34 +0900

    define list node

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -6,10 +6,11 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
 class Solution(object):
     def convertToNumber(self, ls):
         length = len(ls)
