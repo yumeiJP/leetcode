821c38b8b2f06a952e644f8b9e9472d410714757
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-03 21:35:31 +0900

    238 product of array except self

--- 238.product-of-array-except-self.py
+++ 238.product-of-array-except-self.py
@@ -11,6 +11,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+
+        n = len(nums)
+
+        prefix = [1]*n
+        suffix = [1]*n
+        output = [1]*n
+
+        prefix[0] = 1
+        suffix[n-1] = 1
+
+        for i in range(1, n):
+            prefix[i] = prefix[i-1] * nums[i-1]
+        
+        for i in range(n-2, -1, -1):
+            suffix[i] = nums[i+1]*suffix[i+1]
+        
+        for i in range(n):
+            output[i] = prefix[i] * suffix[i]
+        
+        return output
+
+
         
 # @lc code=end
 
