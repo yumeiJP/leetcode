f1545d096165653bf7c3923f99e5b22e747d878e
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-04 14:57:32 +0900

    167 two sum ii input array is suported

---
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=167 lang=python
+#
+# [167] Two Sum II - Input Array Is Sorted
+#
+
+# @lc code=start
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        n = len(numbers)
+        l = 0
+        r = n-1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
+            else:
+                return [l+1,r+1]
+        
+# @lc code=end
+
