ce1b046d99287ac47b7307034142c5a3dec7b01a
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-08 17:08:45 +0900

    875 koko eating bananas

---
+++ 875.koko-eating-bananas.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=875 lang=python
+#
+# [875] Koko Eating Bananas
+#
+
+# @lc code=start
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        
+# @lc code=end
+
