27ac70be9cf8c896ad9a6638a1308fe5af52f863
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-09 10:54:35 +0900

    875 koko eating bananas

--- 875.koko-eating-bananas.py
+++ 875.koko-eating-bananas.py
@@ -5,6 +5,9 @@
 #
 
 # @lc code=start
+
+import math
+
 class Solution(object):
     def minEatingSpeed(self, piles, h):
         """
@@ -12,6 +15,31 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
+
+        n = len(piles)
+        l=0
+        r=piles[-1]
+        best = float('inf')
+
+        while l<r:
+            
+        
+        
+
+        
+
+
         
 # @lc code=end
+"""
+[4, 11, 20, 23, 30], h = 6
 
+k=20 --> 12 hours --> 12 > 6
+
+shes eating too slow --> k=23 --> 
+
+if too fast, k = 11, if too slow, 23
+binary search
+
+
+"""
