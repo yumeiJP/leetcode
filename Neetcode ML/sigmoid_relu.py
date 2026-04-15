2652821dfb9629df40995336e631924773fc14ff
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-15 12:36:56 +0900

    sigmoid relu

---
+++ Neetcode ML/sigmoid_relu.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+        return 1/(1+math.e**(-z))
+
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        return max(0,z)
