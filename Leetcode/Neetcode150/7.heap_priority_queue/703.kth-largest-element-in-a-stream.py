546c75d58984f072ed2f01e67457ad7dedd32e2c
yumeiJP <tk5138@email.kist.ed.jp>
2026-04-15 21:40:02 +0900

    e

---
+++ Leetcode/Neetcode150/7.heap_priority_queue/703.kth-largest-element-in-a-stream.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=703 lang=python
+#
+# [703] Kth Largest Element in a Stream
+#
+
+# @lc code=start
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+# @lc code=end
+
